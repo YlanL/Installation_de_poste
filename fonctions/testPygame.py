2a22d2bafaae0a362adce3d982789c9c993de6ab
lodi0003 <ylan.lodi@etudiant.univ-reims.fr>
2023-10-26 19:07:46 +0200

    Upload New File

---
+++ fonctions/testPygame.py
@@ -0,0 +1,8 @@
+import pygame,sys
+from pygame.locals import *
+pygame.init()
+fenetre=pygame.display.set_mode((640,480))
+fenetre.fill([10,186,180])
+pygame.display.flip()
+while True:
+        pass
