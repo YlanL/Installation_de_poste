892d294b815953d92e107c69fd469a307bc59f41
lodi0003 <ylan.lodi@etudiant.univ-reims.fr>
2023-10-26 19:06:35 +0200

    Upload New File

---
+++ fonctions/fonctionPylint.py
@@ -0,0 +1,8 @@
+VARIABLE=21
+i=0
+ChiffresStockésDansUneListe=[4,21,6,3,3,2,3]
+while i<len(ChiffresStockésDansUneListe):
+        if ChiffresStockésDansUneListe[i]==VARIABLE:
+                break
+        i=i+1
+        print(i)
